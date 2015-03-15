a28af8a7f670e06f7c04340d673cd723ea0b1c23
Ricardo Champa <Ricardo@MacR.local>
2015-03-15 16:35:53 +0100

    entrega 2 completa

--- entrega2.js
+++ entrega2.js
@@ -1,4 +1,4 @@
-db.entrega2.drop();
+db.media.drop();
 
 libros = { "tipo": "libro",
  "titulo": "Java para todos",
@@ -49,39 +49,40 @@ DVD = { "tipo": "DVD",
 
 // EJERCICIO 1
 listabulk=[libros, CD, DVD]; //Las listas en javascript usan corchetes
-db.entrega2.insert(listabulk);
+db.media.insert(listabulk);
 
 //EJERCICIO 2
-db.entrega2.find({"Titulo": "Matrix"} , {actores: 1 ,_id: 0})
+db.media.find({"Titulo": "Matrix"} , {actores: 1 ,_id: 0})
 
 //EJERCICIO 3
-db.entrega2.find({"Titulo": "Matrix"} , {actores: 0})
+db.media.find({"Titulo": "Matrix"} , {actores: 0})
 
 //EJERCICIO 4
-db.entrega2.find({"Titulo": "Matrix"} , {tipo: 1, Titulo:1 , _id:0})
+db.media.find({"Titulo": "Matrix"} , {tipo: 1, Titulo:1 , _id:0})
 
 //EJERCICIO 5
 //No me lo muestra bien creo que es por la tilde??''
-db.entrega2.find({"tipo": "libro", "editorial": "Anaya"} , {capítulos: 1})
+db.media.find({"tipo": "libro", "editorial": "Anaya"} , {"capítulos": 1})
 
-//EJERCICIO 6db.entrega2.find({"canciones.titulo":"Pajaritos"})
+//EJERCICIO 6
+db.media.find({"canciones.titulo":"Pajaritos"})
 
 //EJERCICIO 7
-db.entrega2.find({"Autor":"Timoteo Marino"})
+db.media.find({"Autor":"Timoteo Marino"})
 //alternativamente
-db.entrega2.find({"Autor" : {"$in": ["Timoteo Marino"]}})
+db.media.find({"Autor" : {"$in": ["Timoteo Marino"]}})
 
 //EJERCICIO 8
-db.entrega2.find().sort({tipo : -1})
+db.media.find().sort({tipo : -1})
 
 //EJERCICIO 9
-db.entrega2.find().limit(2).sort({tipo : -1}
+db.media.find().limit(2).sort({tipo : -1})
 
 //EJERCICIO 10
-db.entrega2.find().limit(2).skip(1).sort({tipo : -1})
+db.media.find().limit(2).skip(1).sort({tipo : -1})
 
 //EJERCICIO 11
-db.entrega2.find().limit(2).skip(1).sort({tipo : -1})
+db.media.find().limit(2).skip(2).sort({tipo : -1})
 
 
 //EJERCICIO 12
@@ -96,4 +97,14 @@ doc2 = { "tipo" : "DVD",
 }
 
 extrabulk=[doc1, doc2]; 
-db.entrega2.insert(extrabulk);
+db.media.insert(extrabulk);
+//12.1
+db.media.find({"estreno": { $gt: 2000 }} , {actores: 0})
+//12.2
+db.media.find({"estreno": { $gte: 1999 }} , {actores: 0})
+//12.3
+db.media.find({"estreno": { $lt: 1999 }} , {actores: 0})
+//12.4
+db.media.find({"estreno": { $lte: 1999 }} , {actores: 0})
+//12.5
+db.media.find({"estreno": { $gte: 1999, $lt: 2010 }} , {actores: 0})
