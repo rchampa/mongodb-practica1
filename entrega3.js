77a8989242184021c37750c5cca83230f633590d
Max <you@example.com>
2015-03-22 16:37:29 +0100

    impares hechos el 14 va pero el 14 no y en teoria creo que es solo poner una negacion

--- entrega3.js
+++ entrega3.js
@@ -6,8 +6,8 @@ libro = { "tipo": "libro",
  "editorial": "Anaya",
  "Autor": ["Pepe Caballero",
  "Isabel Sanz","Timoteo Marino",
- "Camilo José Cela"],
-"capítulos": [
+ "Camilo Jose Cela"],
+"capitulos": [
  {"capitulo":1,
  "titulo":	
  "Primeros pasos en Java", 
@@ -58,12 +58,24 @@ DVD2 = { "tipo": "DVD",
  ]
 }
 
-lista= [libro, CD, DVD, DVD2];
+//Insercion extra
+CD2 = { "tipo": "CD",
+"Artista":"Los piratas",
+"Titulo": "Recuerdos",
+"canciones": [{"cancion":1,
+"titulo":"Adios mi barco",
+"longitud": "3:20"},
+{"cancion":3
+,
+"titulo":"Pajaritos",
+"longitud": "4:15" }]}
+
+lista= [libro, CD, CD2, DVD, DVD2];
 db.media.insert(lista);
 
 
 //Ejercicio 1
-db.media.find({"tipo" : "libro", "Autor" : {"$ne" : "Camilo José Cela"}})
+db.media.find({"tipo" : "libro", "Autor" : {"$ne" : "Camilo JosÃ© Cela"}})
 
 //Ejercicio 2
 db.media.find({"tipo" : "DVD", "estreno" : {"$in" : [1999,2005,2006]}} , {"actores" : 0})
@@ -74,4 +86,34 @@ db.media.find({"tipo" : "DVD", "estreno" : {"$nin" : [1999,2005,2009]}} , {"acto
 //Ejercicio 4 
 db.media.find({"tipo" : "DVD", "estreno" : {"$in" : [1999,2005]}} , {"actores" : 0})
 
+//Ejercicio 5
+db.media.find({"tipo" : "libro", "Autor" : {"$in" : ["Pepe Caballero", "Isabel Sanz"]}, "capitulos.titulo" : "Bucles"})
 
+//Ejercicio 7
+//3 primeros
+db.media.find({"Titulo": "Matrix"}, { "actores" : {"$slice" : 3}})
+//3 ultimos
+db.media.find({"Titulo": "Matrix"}, { "actores" : {"$slice" : -3}})
+//3 primeros saltandose los 2 primeros
+db.media.find({"Titulo": "Matrix"}, { "actores" : {"$slice" : [2,3]}})
+//4 actores saltandose los 5 ultimos
+db.media.find({"Titulo": "Matrix"}, { "actores" : {"$slice" : [-5,4]}})
+
+//Ejercicio 9
+db.media.find({"tipo" : "DVD" ,"estreno" : {"$mod" : [2,1]}}, {"actores" : 0}) // [divisor, resto]
+
+//Ejercicio 11
+db.media.find({"actores" : {"$ne" : null}}) // 
+
+//Ejercicio 13
+db.media.find({"canciones" : {"$type" : 3}})
+
+//Ejercicio 14 
+//Insertamos el nuevo documentos CD2
+db.media.find({"tipo" : "CD" ,"canciones" : {"$elemMatch" : {"titulo" : "Pajaritos", "cancion" : 2}}})
+
+//Ejercicio 15 // NO SE PORQUE NO FUNCIONA??''' en teoria con negarlo iria
+db.media.find({"tipo" : "CD" ,"canciones" : { "$ne" : {"$elemMatch" :  {"titulo" : "Pajaritos", "cancion" : 2}}}})
+
+//Ejercicio 17
+db.media.find({"Artista" : /piratas/i })
