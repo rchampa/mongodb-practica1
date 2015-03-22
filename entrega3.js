ecbe789df2fc5331291aebe2e1b2502e0856597d
Ricardo Champa <ricardochampa@gmail.com>
2015-03-22 21:19:32 +0100

    emtrega 3 y reordenamiento

--- entrega3.js
+++ entrega3.js
@@ -4,73 +4,55 @@ libro = { "tipo": "libro",
  "titulo": "Java para todos",
  "ISBN": "987-1-2344-5334-8",
  "editorial": "Anaya",
- "Autor": ["Pepe Caballero",
- "Isabel Sanz","Timoteo Marino",
- "Camilo Jose Cela"],
-"capitulos": [
- {"capitulo":1,
- "titulo":	
- "Primeros pasos en Java", 
- "longitud": 20
- },
- {"capitulo":2,
- "titulo":"Bucles",
- "longitud": 25
- }
- ]
+ "Autor": ["Pepe Caballero", "Isabel Sanz","Timoteo Marino", "Camilo Jose Cela"],
+"capitulos": [ {"capitulo":1, "titulo": "Primeros pasos en Java", "longitud": 20 },
+ {"capitulo":2, "titulo":"Bucles", "longitud": 25 } ]
 }
 CD = { "tipo": "CD",
  "Artista":"Los piratas",
  "Titulo": "Recuerdos",
- "canciones": [
- {"cancion":1,
- "titulo":"Adios mi barco",
- "longitud": "3:20"
- },
- {"cancion":2,
- "titulo":"Pajaritos",
- "longitud": "4:15"
- }
- ]
+ "canciones": [ {"cancion":1, "titulo":"Adios mi barco", "longitud": "3:20" },
+ {"cancion":2, "titulo":"Pajaritos", "longitud": "4:15" } ]
 }
 DVD = { "tipo": "DVD",
  "Titulo": "Matrix",
  "estreno": 1999,
- "actores": [
- "Keanu Reeves",
- "Carry-Anne Moss",
- "Laurence Fishburne",
- "Hugo Weaving",
- "Gloria Foster",
- "Joe Pantoliano"
- ]
+ "actores": [ "Keanu Reeves", "Carry-Anne Moss", "Laurence Fishburne", "Hugo Weaving", "Gloria Foster", "Joe Pantoliano" ]
 }
 DVD2 = { "tipo": "DVD",
  "Titulo": "Matrix Reloaded",
  "estreno": 2000,
- "actores": [
- "Keanu Reeves",
- "Carry-Anne Moss",
- "Laurence Fishburne",
- "Hugo Weaving",
- "Gloria Foster",
- "Joe Pantoliano"
- ]
+ "actores": [ "Keanu Reeves", "Carry-Anne Moss", "Laurence Fishburne", "Hugo Weaving", "Gloria Foster", "Joe Pantoliano" ]
+}
+DVD3 = { "tipo": "DVD",
+ "Titulo": "Recuerdos",
+ "estreno": 2000,
+ "actores": [ "Keanu Reeves", "Carry-Anne Moss", "Laurence Fishburne", "Hugo Weaving", "Gloria Foster", "Joe Pantoliano" ]
+}
+DVD4 = { "tipo": "DVD",
+ "Titulo": "Recuerdos",
+ "estreno": 1994,
+ "actores": [ "Keanu Reeves", "Carry-Anne Moss", "Laurence Fishburne", "Hugo Weaving", "Gloria Foster", "Joe Pantoliano" ]
 }
 
 //Insercion extra
 CD2 = { "tipo": "CD",
 "Artista":"Los piratas",
 "Titulo": "Recuerdos",
-"canciones": [{"cancion":1,
-"titulo":"Adios mi barco",
-"longitud": "3:20"},
-{"cancion":3
-,
-"titulo":"Pajaritos",
-"longitud": "4:15" }]}
+"canciones": [{"cancion":1,"titulo":"Adios mi barco","longitud": "3:20"},
+{"cancion":3,"titulo":"Pajaritos","longitud": "4:15" }]}
 
-lista= [libro, CD, CD2, DVD, DVD2];
+//Insercion extra
+CD3 = { "tipo": "CD",
+"Artista":"Los piratas",
+"Titulo": "Recuerdos",
+"canciones": [
+{"cancion":1,"titulo":"Adios mi barco","longitud": "3:20"},
+{"cancion":3,"titulo":"Pajaritos","longitud": "4:15" },
+{"cancion":4,"titulo":"Pajaritos4","longitud": "44:15" }
+]}
+
+lista= [libro, CD, CD2, CD3, DVD, DVD2,DVD3, DVD4];
 db.media.insert(lista);
 
 
@@ -89,6 +71,9 @@ db.media.find({"tipo" : "DVD", "estreno" : {"$in" : [1999,2005]}} , {"actores" :
 //Ejercicio 5
 db.media.find({"tipo" : "libro", "Autor" : {"$in" : ["Pepe Caballero", "Isabel Sanz"]}, "capitulos.titulo" : "Bucles"})
 
+//Ejercicio 6
+db.media.find({"tipo" : "DVD", $or:[{"estreno":"1999"}, {"Titulo" : "Recuerdos"}]})
+
 //Ejercicio 7
 //3 primeros
 db.media.find({"Titulo": "Matrix"}, { "actores" : {"$slice" : 3}})
@@ -99,11 +84,20 @@ db.media.find({"Titulo": "Matrix"}, { "actores" : {"$slice" : [2,3]}})
 //4 actores saltandose los 5 ultimos
 db.media.find({"Titulo": "Matrix"}, { "actores" : {"$slice" : [-5,4]}})
 
+//Ejercicio 8
+db.media.find ({"tipo" : "DVD" , "estreno" : { "$mod" : [ 2, 0 ] } } , { "actores" : 0})
+
 //Ejercicio 9
 db.media.find({"tipo" : "DVD" ,"estreno" : {"$mod" : [2,1]}}, {"actores" : 0}) // [divisor, resto]
 
+//Ejercicio 10
+db.media.find({ "canciones": {"$exists": true}, "canciones":{"$size": 2 } } )
+
 //Ejercicio 11
-db.media.find({"actores" : {"$ne" : null}}) // 
+db.media.find({"actores" : {"$ne" : null}}) 
+
+//Ejercicio 12
+db.media.find({ "actores": {"$exists": false} } )
 
 //Ejercicio 13
 db.media.find({"canciones" : {"$type" : 3}})
@@ -115,5 +109,11 @@ db.media.find({"tipo" : "CD" ,"canciones" : {"$elemMatch" : {"titulo" : "Pajarit
 //Ejercicio 15 // NO SE PORQUE NO FUNCIONA??''' en teoria con negarlo iria
 db.media.find({"tipo" : "CD" ,"canciones" : { "$ne" : {"$elemMatch" :  {"titulo" : "Pajaritos", "cancion" : 2}}}})
 
+//Ejercicio 16
+db.media.find ({"tipo" : "DVD" , "estreno" : { "$lt" : 1995 } })
+
 //Ejercicio 17
 db.media.find({"Artista" : /piratas/i })
+
+//Ejercicio 18
+db.media.find({"Artista" : /piratas/i , "Titulo": {"$exists": true}})
