010448e98bcdf2d4439a30a1f23a635c3d37eb7a
Ricardo Champa <ricardochampa@gmail.com>
2015-04-19 21:25:46 +0200

    explaaaaaaaain

--- entrega5.js
+++ entrega5.js
@@ -1,3 +1,5 @@
+db.prueba.drop();
+
 for($i=1; $i<100; $i++){
 	db.prueba.insert({
 		"_id":ObjectId(),
@@ -11,7 +13,31 @@ for($i=1; $i<100; $i++){
 		"_id":ObjectId(),
 		"producto":"sillas",
 		"num-a":$i*3,
-		"num-b":$*i,
+		"num-b":$i,
 		"num-c":$i*2
 	});
 }
+
+uno = db.prueba.aggregate(
+	{$project: {a_gt_b: {$cmp: ['$num-a','$num-b']}}},
+    {$match: {a_gt_b:{$gt:0}}},
+    {$group:{"_id":"$a_gt_b",total:{"$sum":1}}},
+    {$project: {"_id":0,"total":1}}
+);
+
+// dos = db.prueba.aggregate(
+// 	{$project: {a_gt_b: {$cmp: ['$num-a','$num-b']}}},
+//     {$match: {a_gt_b:{$gt:0}}},
+//     {$group:{"_id":"$a_gt_b",total:{"$sum":1}}},
+//     {$project: {"_id":0,"total":1}}
+// ).explain();
+
+
+dos = db.prueba.aggregate([
+   {$project: {a_gt_b: {$cmp: ['$num-a','$num-b']}}},
+    {$match: {a_gt_b:{$gt:0}}},
+    {$group:{"_id":"$a_gt_b",total:{"$sum":1}}},
+    {$project: {"_id":0,"total":1}}
+], {
+   explain: true
+})
